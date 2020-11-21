2d4d455b131c91b34d316205bc519fb787aa4ab3
Keiser <827401565@qq.com>
2020-11-21 18:38:02 +0800

    Updated by Keiser: 在MainActivity新建【临时登录按钮】作为LoginActivity的启动； 点击用户注册跳转RegisterActivity，目前只把布局构件建立，实现城市、学校选择功能，其他稍后完善

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -35,5 +35,9 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("words",data)
             startActivity(intent)
         }
+        temp_loginButton.setOnClickListener(){
+            val intent=Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
